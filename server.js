88ca94f0ca5faae54dfa16d1979de140b49810ba
Raphael Piccolo <rafi.piccolo@gmail.com>
2018-10-06 17:11:56 +0200

    x

---
+++ server.js
@@ -0,0 +1,45 @@
+var moment = require('moment');
+var request = require('request');
+var exec = require('child_process').execFile;
+var jar = request.jar();
+
+function isRdvDisponible(callback) {
+    request({
+        url: 'http://www.seine-saint-denis.gouv.fr/booking/create/2616/0',
+        method: 'post',
+        form: {
+            condition: 'on',
+            nextButton: 'Effectuer une demande de rendez-vous',
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+            'Referer': 'http://www.seine-saint-denis.gouv.fr/booking/create/2616/0'
+        },
+        jar: jar
+    }, function(err, response, body) {
+        if (err) return callback('erreur grave :) '+err.code+' '+err.message);
+        if (response.statusCode != 200) return callback('Le site est planté : code '+response.statusCode);
+        
+        // analyse la page obtenue
+        body = body.toLowerCase();
+        var nordv = body.indexOf('plus de plage horaire') != -1;
+        var formulairevisible = body.indexOf('nextButton') != -1;
+        var plante = body.indexOf('site indisponible') != -1 || body.indexOf('maintenance') != -1;
+
+        if (plante) return callback('Le site est en maintenance');
+        else if (nordv) return callback('pas de rdv disponible');
+        else return callback('RDV disponible !!!', 1);
+    });
+}
+
+function startWatch() {
+    var start = new Date();
+    isRdvDisponible(function(message, ok) {
+        var end = new Date();
+        console.log(moment().format('YYYY-MM-DD HH:mm:ss')+' '+(String(end-start).padStart(6, ' '))+'ms '+message);
+        if (ok) exec('/usr/bin/say', ['rdv disponible'], function() {});
+        setTimeout(startWatch, 5000)
+    });
+}
+
+startWatch();
